f1d643af7036563411f7f89176f4780c5332b559
Jo941018 <jsanchez@quimicamarel.com.mx>
2021-11-24 21:23:38 -0600

    Enviando apuntes de la clase

--- js/script.js
+++ js/script.js
@@ -2,3 +2,15 @@
 
 
 console.log('Hola Mundo, desde mi portafolio');
+
+
+function Multiplica(a,b) {
+    let resultado = 0;
+    for (let index = 1; index <= b; index++) {
+        resultado = resultado + a;
+        
+    }
+    return resultado
+}
+
+console.log(mult(7,15));
